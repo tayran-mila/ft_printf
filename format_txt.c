33ccedb4b535253ca2c15e6eff7a975345929157
tayran-mila <tmendes-@student.42sp.org.br>
2020-07-19 16:53:38 -0300

    Development of the Flags logic

--- format_txt.c
+++ format_txt.c
@@ -6,27 +6,76 @@
 /*   By: tmendes- <tmendes-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/18 08:20:06 by tmendes-          #+#    #+#             */
-/*   Updated: 2020/07/18 15:34:10 by tmendes-         ###   ########.fr       */
+/*   Updated: 2020/07/19 16:44:05 by tmendes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
+
+char	*repeat_chr(char chr, int nbr)
+{
+	char	*str;
+	int		k;
+
+	if (!(str = (char *)malloc((nbr + 1) * sizeof(char))))
+		return (NULL);
+	k = 0;
+	while (k < nbr)
+	{
+		*(str + k) = chr;
+		k++;
+	}
+	*(str + k) = 0;
+	return (str);
+}
+
+char	*pad_str(char *str, t_fields fld)
+{
+	fld.itg = fld.width - ft_strlen(str);
+	if (fld.itg > 0)
+	{
+		if (((fld.flag/10000)%10))
+			fld.str = repeat_chr(' ', fld.itg);
+		else
+		{
+			fld.str = str;
+			if (((fld.flag/1000)%10))
+			{
+				str = repeat_chr('0', fld.itg);
+				if (*fld.str == '-' || *fld.str == ' ' || *fld.str == '+')
+				{
+					*str = *fld.str;
+					*fld.str = '0';
+				}
+			}
+			else
+				str = repeat_chr(' ', fld.itg);
+		}
+		str = join_ptr(str, fld.str);
+		free(fld.str);
+		fld.str = NULL;	
+	}
+	return (str);
+}
+
+
 
 char *format_txt(t_printf ptf, va_list ap)
 {
 	t_fields	fld;
 
 	fld = set_fields(ptf.begin, ptf.end, fld);
-
 	if (*ptf.end == 'c')
 	{
 		ptf.txt = ft_strdup("");
 		*ptf.txt = (char) va_arg(ap, int);
+		ptf.txt = pad_str(ptf.txt, fld);
 		return (ptf.txt);
 	}
 	if (*ptf.end == 's')
 	{
 		ptf.txt = va_arg(ap, char *);
+		ptf.txt = pad_str(ptf.txt, fld);
 		return (ptf.txt);
 	}
 	if (*ptf.end == 'p')
@@ -36,26 +85,43 @@ char *format_txt(t_printf ptf, va_list ap)
 		ptf.txt = join_ptr(ptf.txt, fld.str);
 		free (fld.str);
 		fld.str = NULL;
+		ptf.txt = pad_str(ptf.txt, fld);
 		return (ptf.txt);
 	}
 	if (*ptf.end == 'd' || *ptf.end == 'i')
 	{
 		ptf.txt = ft_llitoa(va_arg(ap, int), 10, 'a');
+		ptf.txt = pad_str(ptf.txt, fld);
 		return (ptf.txt);
 	}
 	if (*ptf.end == 'u')
 	{
 		ptf.txt = ft_llitoa(va_arg(ap, unsigned int), 10, 'a');
+		ptf.txt = pad_str(ptf.txt, fld);
 		return (ptf.txt);
 	}
 	if (*ptf.end == 'x')
 	{
-		ptf.txt = ft_llitoa(va_arg(ap, long long int), 16, 'a');
+		ptf.txt = ft_strdup("");
+		if ( (fld.flag%10) == 1)
+			ptf.txt = join_ptr(ptf.txt, "0x");
+		fld.str = ft_llitoa(va_arg(ap, long long int), 16, 'a');
+		ptf.txt = join_ptr(ptf.txt, fld.str);
+		free (fld.str);
+		fld.str = NULL;
+		ptf.txt = pad_str(ptf.txt, fld);
 		return (ptf.txt);
 	}
 	if (*ptf.end == 'X')
 	{
-		ptf.txt = ft_llitoa(va_arg(ap, long long int), 16, 'A');
+		ptf.txt = ft_strdup("");
+		if ( (fld.flag%10) == 1)
+			ptf.txt = join_ptr(ptf.txt, "0X");
+		fld.str = ft_llitoa(va_arg(ap, long long int), 16, 'A');
+		ptf.txt = join_ptr(ptf.txt, fld.str);
+		free (fld.str);
+		fld.str = NULL;
+		ptf.txt = pad_str(ptf.txt, fld);
 		return (ptf.txt);
 	}
 	if (*ptf.end == '%')
@@ -79,10 +145,15 @@ char *format_txt(t_printf ptf, va_list ap)
 	{
 		fld.flt = (long double)va_arg(ap, double);
 		ptf.txt = ft_ftoa( fld.flt, fld.prec);
+		if ( fld.prec == 0 && fld.flag%10 == 0)
+			ptf.txt = unpad(ptf.txt, 'f');
+		ptf.txt = pad_str(ptf.txt, fld);
 		return (ptf.txt);
 	}
 	if (*ptf.end == 'g')
 	{
+		if ( fld.prec == 0)
+			fld.prec++;
 		fld.flt = (long double)va_arg(ap, double);
 		fld.itg = nbr_exp(fld.flt);
 		if (fld.itg < -4 || fld.itg >= fld.prec)
@@ -98,12 +169,17 @@ char *format_txt(t_printf ptf, va_list ap)
 				fld.itg = -fld.itg;
 			fld.str = ft_ftoa(fld.flt, fld.itg + fld.prec + 1);
 			fld.str = scntfc_not(fld.str, fld.prec - 1);
-			fld.str = unpad(fld.str, 'e');
+			if ( fld.flag%10 == 0)
+				fld.str = unpad(fld.str, 'e');
 			ptf.txt = join_ptr(ptf.txt, fld.str);
+			free(fld.str);
+			fld.str = NULL;
 		}
 		else
-			ptf.txt = ft_ftoa( fld.flt, fld.prec - (fld.itg + 1));		
-		ptf.txt = unpad(ptf.txt, 'f');
+			ptf.txt = ft_ftoa( fld.flt, fld.prec - (fld.itg + 1));
+		if ( fld.flag%10 == 0)
+			ptf.txt = unpad(ptf.txt, 'f');
+		ptf.txt = pad_str(ptf.txt, fld);
 		return (ptf.txt);
 	}
 	if (*ptf.end == 'e')
@@ -122,6 +198,11 @@ char *format_txt(t_printf ptf, va_list ap)
 		fld.str = ft_ftoa(fld.flt, fld.itg + fld.prec + 1);
 		fld.str = scntfc_not(fld.str, fld.prec);
 		ptf.txt = join_ptr(ptf.txt, fld.str);
+		free(fld.str);
+		fld.str = NULL;
+		if ( fld.prec == 0 && fld.flag%10 == 0)
+			ptf.txt = unpad(ptf.txt, 'e');
+		ptf.txt = pad_str(ptf.txt, fld);
 		return (ptf.txt);
 	}
 	return (NULL);
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tmendes- <tmendes-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 13:12:18 by tmendes-          #+#    #+#             */
-/*   Updated: 2020/07/18 08:07:59 by tmendes-         ###   ########.fr       */
+/*   Updated: 2020/07/19 16:48:26 by tmendes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,9 +30,12 @@ int main()
 	ptr = &nbr;
 
 	//nbr = 12345678901234567890123456789012345678901234567890.1234567890;
-	nbr = 12385678900000000000000000000000000000000000000000.0;
-	//nbr = 999699.0000;
+	nbr = -1238567000000000000.0;
+	nbr = 123;
 	//nbr = -100000000000000000000000000000000000000000.0000;
+
+	//str = repeat_chr('T', 7);
+	//printf ("\n\nRepeat: %s\n\n", str);
 
 	dig = nbr_exp(nbr);
 
@@ -48,9 +51,15 @@ int main()
 	iptr = &nptr;
 	dig = 20;
 	*iptr = 0;
-	print1 = printf("\nOr_Printf: %% hexa: %d \\o/ %n%p %.15g", num, iptr, ptr, nbr);
+	//print1 = printf("\nOr_Printf: %% hexa: %015d \\o/ %n%p %+015.5e", num, iptr, ptr, nbr);
+	//printf(" %d\n",print1);
+	//print2 = ft_printf("\nFt_Printf: %% hexa: %d \\o/ %n%p %+015.5e", num, iptr, ptr, nbr);
+	//printf(" %d\n\n",print2);
+	print1 = printf("\nOr_Printf: %#.6g", nbr);
 	printf(" %d\n",print1);
-	print2 = ft_printf("\nFt_Printf: %% hexa: %d \\o/ %n%p %.15g", num, iptr, ptr, nbr);
+	print2 = ft_printf("\nFt_Printf: %#.6g", nbr);
 	printf(" %d\n\n",print2);
+	ptr++;
+	num++;
 	return(0);
 }
