e48947f168447d4cd4d31216f6c17c3f2799889f
tayran-mila <tmendes-@student.42sp.org.br>
2020-07-28 14:21:08 -0300

    Fixing issue about zero padding and %x conversion

--- ft_printf_conversions.c
+++ ft_printf_conversions.c
@@ -6,7 +6,7 @@
 /*   By: tmendes- <tmendes-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/18 08:20:06 by tmendes-          #+#    #+#             */
-/*   Updated: 2020/07/28 11:49:58 by tmendes-         ###   ########.fr       */
+/*   Updated: 2020/07/28 14:19:25 by tmendes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -223,8 +223,16 @@ t_printf	x_decimal(t_printf ptf, t_fields fld, va_list ap)
 		fld.str = pad_str(fld.str, fld, fld.prec, 'p');
 		fld.flag[3] = 0;
 	}
-	fld.str = pad_str(fld.str, fld, (fld.width - ft_strlen(ptf.txt)), 'w');
-	ptf.txt = ft_concat(ptf.txt, fld.str);
+	if (fld.flag[3])
+	{
+		fld.str = pad_str(fld.str, fld, (fld.width - ft_strlen(ptf.txt)), 'w');
+		ptf.txt = ft_concat(ptf.txt, fld.str);		
+	}
+	else
+	{
+		ptf.txt = ft_concat(ptf.txt, fld.str);
+		ptf.txt = pad_str(ptf.txt, fld, fld.width, 'w');		
+	}
 	free(fld.str);
 	fld.str = NULL;
 	return (ptf);
